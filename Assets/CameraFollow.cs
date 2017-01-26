0a8dbb9f2f411e9daa2badadbdb2889300720fa8
Timothy Ratliff <timothykratliff@gmail.com>
2017-01-26 12:14:31 -0600

    initial

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    //associate this script with a object in the inspector
+    public GameObject player;
+    //what is the offset of the camera to begin with?
+    Vector3 offset;
+
+	void Start ()
+    {
+        offset = transform.position - player.transform.position;
+	}
+	//using lateUpdate prevents lag
+    //lateUpdate is the last function called on the game
+	void lateUpdate ()
+    {
+        transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/ColorSelector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorSelector : MonoBehaviour
+{
+    SpriteRenderer renderer;
+    void Start()
+    {
+        renderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+    void Update ()
+    {
+        //Debug.Log("Update!");
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            renderer.color = Color.red;
+        }
+        //if (Input.GetKeyUp(KeyCode.R))
+        //{
+        //    gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+        //}
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            renderer.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            renderer.color = Color.blue;
+        }
+    }
+}
